cb3f0abe81c17ea0f81c3f10207024f8f0554d8a
digi-reaper <abhishek@digifutura.com>
2016-07-18 08:44:06 +0530

    changes

--- js/basic.js
+++ js/basic.js
@@ -1,18 +1,34 @@
 function hilfskraft(){
   this.APP         = "Hilfskraft";
+  
   this.animation = function() {
     $('.head_intro').css({"background-size":"110% 110%"});
     $(".element").typed({
       strings: ["Hello", "Ola", "Bonjour", "Hola!", "Ciao"],
     });
   }
+
+  this.logofy = function() {
+    if(document.body.scrollTop>50||document.documentElement.scrollTop>50){
+      $('.logo').addClass('minified');
+    }
+    else{
+      $('.logo').removeClass('minified');
+    
+    }
+  }
 }
 
 $(function(){
   $(".trans-on-load").css({'top':'0px'});
   var myapp = new hilfskraft();
-  myapp.animation()
+  myapp.animation();
 });
+
+window.onscroll=function(){
+  var myapp = new hilfskraft();
+  myapp.logofy();
+}
 
 
 
